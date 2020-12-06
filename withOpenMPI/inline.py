bf752a452376e77550d51465313d43aa36bd04f7
cindytsai <6188366+cindytsai@users.noreply.github.com>
2020-12-06 20:06:22 +0800

    Using mpi4py in python script inside def yt_inline().

--- withOpenMPI/inline.py
+++ withOpenMPI/inline.py
@@ -10,8 +10,27 @@ print ("----------------------")
 print ("prop1 exist = %s" % ("prop1" in dir(libyt)))
 print ("prop2 exist = %s" % ("prop2" in dir(libyt)))
 print ("prop3 exist = %s" % ("prop3" in dir(libyt)))
-print (libyt.prop1)
+print ("----------------------")
+print ("before yt_set_parameter(), libyt.prop1 = ", libyt.prop1)
 
 def yt_inline():
-	print("myrank = ", myrank)
-	print (libyt.prop1)
+	print ("======================")
+	print ("myrank = ", myrank)
+	print ("after yt_set_parameter, libyt.prop1 = ", libyt.prop1)
+	
+	## gather information from other rank
+	################################################
+	libyt.prop2 = MPI.COMM_WORLD.gather(libyt.prop1, root=0)
+	
+	## broadcast libyt.prop1 in rank0 to other ranks
+	################################################
+	# libyt.prop2 = MPI.COMM_WORLD.bcast(libyt.prop1, root=0)	
+	# print ("libyt.prop2 = ", libyt.prop2)
+	
+	if myrank == 0:
+		print ("----------------------")
+		print ("gather information libyt.prop1 from other ranks, ")
+		print ("and stored in libyt.prop2...")
+		print ("libyt.prop2 = ", libyt.prop2)
+
+
